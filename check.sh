5ca23fe6b8c1a989f45645cd5f4e7a1fee93ca62
Alejandro Rodriguez <a.rodrimatt@gmail.com>
2018-05-31 14:26:37 -0300

    hello world

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+CHECK_URL="https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+LATEST_VERSION=$(curl --silent $CHECK_URL)
+SLACK_PAYLOAD="{\"channel\": \"#$SLACK_CHANNEL\", \"text\": \"New Chromedriver version: $LATEST_VERSION\"}"
+
+if [ "$LATEST_VERSION" != "$CURRENT_CHROMEDRIVER_VERSION" ]; then
+    echo "Theres a new chromedriver version: $LATEST_VERSION"
+    curl -s -m 30 -X POST --data-urlencode payload=SLACK_PAYLOAD $SLACK_WEBHOOK
+fi
