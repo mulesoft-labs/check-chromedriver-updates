7e7d6d7a6b66a5295f934e3c37b27cc69bac21d0
Alejandro Rodriguez <a.rodrimatt@gmail.com>
2018-05-31 16:02:49 -0300

    fix slack notification payload

--- check.sh
+++ check.sh
@@ -4,9 +4,10 @@ set -e
 
 CHECK_URL="https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
 LATEST_VERSION=$(curl --silent $CHECK_URL)
-SLACK_PAYLOAD="{\"channel\": \"#$SLACK_CHANNEL\", \"text\": \"New Chromedriver version: $LATEST_VERSION\"}"
+PAYLOAD="{\"channel\": \"$SLACK_CHANNEL\", \"text\": \"New Chromedriver version released: $LATEST_VERSION!\"}"
 
 if [ "$LATEST_VERSION" != "$CURRENT_CHROMEDRIVER_VERSION" ]; then
-    echo "Theres a new chromedriver version: $LATEST_VERSION"
-    curl -s -m 30 -X POST --data-urlencode payload=$SLACK_PAYLOAD $SLACK_WEBHOOK
+    echo "There's a new chromedriver version: $LATEST_VERSION"
+    echo "Sending Slack Notification to channel #$SLACK_CHANNEL"
+    curl -s -d "payload=$PAYLOAD" $SLACK_WEBHOOK
 fi
